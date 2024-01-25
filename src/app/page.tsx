732f995a329f0bfed8c43cb40447d8863223d2f1
solensa <39489819+solensa@users.noreply.github.com>
2024-01-25 23:37:32 +0000

    acrolist start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,129 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useRef } from "react";
+const users = [
+  {
+    abb: "A&F",
+    exp: "Architecture & Facades",
+    desc: "A discipline within the Technical function who’s focus is the technical aspects of the architecture, internal fit out, and building envelope elements of a project.",
+    id: 1,
+  },
+  { abb: "AAFR", exp: "All accident frequency ratio", desc: "", id: 2 },
+  {
+    abb: "Accruals",
+    exp: "",
+    desc: "Liabilities to pay for goods or services that have been received or supplied but are not yet paid for or invoiced. The costs have been incurred but not yet paid for and are not recorded in the job cost ledger i.e. do not appear as a cost transaction in job cost ledgers. There are two types of Accruals: 1. Manual Accrual – is a cost incurred where a GRN/delivery docket has not been processed into the system. Subcontract Accruals are a type of manual accruals. This is when work is performed by Subcontractors and not processed via a PPC or RCTI in COINS 2. COINS system accruals (GRN’s) – show on some job cost reports and reflect GRN’s/delivery received and processed in COINS but not yet invoiced For Subcontractors, the accrual is the calculated difference between the reported S/C liability and the gross certified.",
+    id: 3,
+  },
+  { abb: "SME", exp: "Small Medium Enterprise", desc: "", id: 4 },
+  { abb: "LOR", exp: "Laing O'Rourke", desc: "A construction company.", id: 5 },
+  {
+    abb: "DfMA",
+    exp: "Design for Manufacture and Assembly ",
+    desc: "A way of designing and constructing a quality building more quickly, efficiently and sustainably. Using a DfMA approach means that elements of a building are created in manufacturing environment rather than on site, and to more standard designs and specifications.",
+    id: 6,
+  },
+  { abb: "CAT", exp: "", desc: "A small furry animal that believes it rules the world.", id: 7 },
+  {
+    abb: "DOG",
+    exp: "",
+    desc: "A small / medium sized waggy animal that is always happy to see you.",
+    id: 8,
+  },
+  { abb: "Acrolist", exp: "", desc: "A tool to help you navigate acronyms.", id: 9 },
+  { abb: "zsdnfvjkdfb", exp: "ojsdnfvjdfb", desc: "When you sleep on your keyboard.", id: 10 },
+];
 
 export default function Home() {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [showNav, setShowNav] = useState(false);
+  const [showList, setShowList] = useState(false);
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchItem(searchTerm);
+    const filteredItems = users.filter(
+      (user) =>
+        user.abb.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.exp.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    // if there is no search term, hide the list
+    if (searchTerm === "") {
+      setShowList(false);
+    } else {
+      setShowList(true);
+    }
+    setFilteredUsers(filteredItems);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="lor-grey flex items-center justify-between flex-wrap p-3">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <img ref={imageRef} className="fill-current h-8  mr-2" src={"/lor.png"} alt="lor" />
+          {/* <span className="font-semibold text-xl tracking-tight">LOR Learn</span> */}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <div className="block">
+          <button
+            className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
+            onClick={() => setShowNav(!showNav)}
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow ">
+          <div className={(showNav ? "block " : "hidden ") + "text-sm "}>
+            <a href="#responsive-header" className="mt-4  text-gray-200 hover:text-white mr-4">
+              LOR Learn
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4  text-gray-200 hover:text-white mr-4"
+            >
+              iGMS
+            </a>
+          </div>
+        </div>
+      </nav>
+      <main className="white-bg flex min-h-screen flex-col items-center  md:p-24">
+        <img
+          ref={imageRef}
+          className="acrolistTitle m-20 mb-0"
+          src={"/acrolist.svg"}
+          alt="acrolist"
         />
-      </div>
+        <input
+          className="w-11/12 md:w-3/5 h-12 p-4 rounded-lg m-6 sb-grey"
+          type="text"
+          value={searchItem}
+          onChange={handleInputChange}
+          placeholder="Search for acronym or jargon..."
+        />
+        <ul className={(showList ? "block " : "hidden ") + "w-11/12 md:w-3/5"}>
+          {filteredUsers.map((user) => (
+            <li key={user.id} className="box-content  p-4 mt-2 rounded-lg li-blue  ">
+              <div className="strong">{user.abb}</div>
+              <div className="italics mt-2">{user.exp}</div>
+              <div className="mt-2">{user.desc}</div>
+            </li>
+          ))}
+          <li className="box-content  p-4 mt-5 rounded-lg li-red  ">
+            <div className="strong">Can't find what you're looking for?</div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="mt-2">
+              Ask on Yammer here. When you find out, please add it here (need to add a button here)
+            </div>
+          </li>
+        </ul>
+      </main>
+    </>
   );
 }
